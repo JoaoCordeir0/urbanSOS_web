5684c43d7b81058e2e38ee9dac7f09c60b523d47
joao.cordeiro <joao.victor@nubbi.com.br>
2023-10-27 10:55:23 -0300

    Caça ao bug em prod

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue';
 import jwt from 'jsonwebtoken'
 
 const endpointKey = import.meta.env.VITE_KEY_ENDPOINT
-const endpointUrl = import.meta.env.VITE_URL_ENDPOINT  
+const endpointUrl = import.meta.env.VITE_URL_ENDPOINT
 
 export interface ILoginData {
     message: String,
@@ -10,26 +10,27 @@ export interface ILoginData {
 }
 
 export async function apiLogin(username, password) {
-   
+
     const response = await fetch(`${endpointUrl}/user/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
     });
-    
+
     const data = await response.json()
 
     if (data.access_token != undefined) {
-        try {            
-            const decoded = jwt.verify(data.access_token, decryptKey())
-            console.log(decoded, data)
-            // localStorage.setItem('TokenUser', data.access_token)
-            // localStorage.setItem('NameUser', decoded.name)
-            // localStorage.setItem('EmailUser', decoded.email)
-            // localStorage.setItem('CpfUser', decoded.cpf)
-        } catch(err) {
-            console.log("erro" + err)
-        }
+
+        console.log(decryptKey())
+        return
+
+        jwt.verify(data.access_token, 'sua-chave-secreta', (err, decoded) => {
+            if (err) {
+                console.error('Erro ao verificar o token:', err);
+            } else {
+                console.log('Token verificado com sucesso:', decoded);
+            }
+        });       
     }
 
     const loginData = ref<ILoginData[]>(data);
@@ -38,35 +39,31 @@ export async function apiLogin(username, password) {
 }
 
 export function checkAuth(to, from, next) {
-    try 
-    {        
-        verify(localStorage.getItem('TokenUser'), decryptKey())                        
+    try {
+        verify(localStorage.getItem('TokenUser'), decryptKey())
         next()
-    } 
-    catch (e) 
-    {
+    }
+    catch (e) {
         next('/login')
     }
 }
 
-export function getToken() {   
-    try 
-    {
-        const token = localStorage.getItem('TokenUser')            
+export function getToken() {
+    try {
+        const token = localStorage.getItem('TokenUser')
         return token
-    } 
-    catch (e) 
-    {
+    }
+    catch (e) {
         const token = 'null'
-        return token 
-    }    
+        return token
+    }
 }
 
 export function credentials() {
     const urlApi = String(endpointUrl)
     const tokenApi = String(getToken())
-    const headerApi = { "x-access-token" : tokenApi }       
-    
+    const headerApi = { "x-access-token": tokenApi }
+
     return {
         urlApi,
         tokenApi,
@@ -74,12 +71,12 @@ export function credentials() {
     }
 }
 
-function decryptKey() {               
-    let chars = (endpointKey.split('')).reverse()    
+function decryptKey() {
+    let chars = (endpointKey.split('')).reverse()
     let endpointKey_decrypt = ''
 
-    for(let c = 0; c<=chars.length; c+=7){
-        endpointKey_decrypt += chars[c]    
-    }    
+    for (let c = 0; c <= chars.length; c += 7) {
+        endpointKey_decrypt += chars[c]
+    }
     return endpointKey_decrypt
 }
